558e4291cec738c2583523e1a39287aedd2488da
Sai Kumar <ksai44576@gmail.com>
2024-07-06 10:51:19 +0530

    Created a main function to serve Axum app

---
+++ backend/auth-server/src/main.rs
@@ -0,0 +1,43 @@
+pub(crate) mod api;
+pub(crate) mod prelude;
+
+use std::{env::var, net::SocketAddr, path::Path};
+
+use axum::serve;
+use dotenvy::from_path;
+use tokio::net::TcpListener;
+#[macro_use]
+extern crate tracing;
+use crate::prelude::*;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+fn init_tracing() {
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_env("RUST_LOG"))
+        .with(tracing_subscriber::fmt::layer().pretty())
+        .init();
+}
+
+#[tokio::main]
+async fn main() {
+    let envfile = Path::new("")
+        .join("backend")
+        .join("auth-server")
+        .join(".env");
+
+    if let Ok(()) = from_path(&envfile) {
+        init_tracing();
+        info!("Loaded .env file from path: {envfile:?}");
+    } else {
+        panic!("Failed to load .env file");
+    }
+
+    let address = var("ADDRESS")
+        .expect("ADDRESS must be set")
+        .parse::<SocketAddr>()
+        .unwrap();
+
+    let listener = TcpListener::bind(address).await.unwrap();
+
+    serve(listener, app()).await.unwrap();
+}
