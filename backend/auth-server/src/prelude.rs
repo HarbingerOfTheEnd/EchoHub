9288d697adb13ed54b045500aea1d9ccc2cc8e06
Sai Kumar <ksai44576@gmail.com>
2024-07-19 19:07:39 +0530

    Moved fn for better structure

--- backend/auth-server/src/prelude.rs
+++ backend/auth-server/src/prelude.rs
@@ -1,11 +1,20 @@
 use std::{env::var, sync::Arc};
 
-use axum::{routing::get, Router};
+use axum::{routing::post, Router};
+#[cfg(test)]
+use sea_orm::MockDatabase;
 use sea_orm::{Database, DbConn};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 use crate::api::v1::oauth2::authorize;
 
-#[cfg(not(test))]
+pub fn init_tracing() {
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_env("RUST_LOG"))
+        .with(tracing_subscriber::fmt::layer().pretty())
+        .init();
+}
+
 pub async fn get_db_conn() -> Arc<DbConn> {
     let db_url = var("DATABASE_URL").expect("DATABASE_URL is not set");
 
@@ -13,15 +22,15 @@ pub async fn get_db_conn() -> Arc<DbConn> {
 }
 
 #[cfg(test)]
-pub async fn get_db_conn() -> Arc<DbConn> {
-    let db = sea_orm::MockDatabase::new(sea_orm::DatabaseBackend::Postgres);
+pub async fn get_mock_db_conn() -> Arc<DbConn> {
+    let db = MockDatabase::new(sea_orm::DatabaseBackend::Postgres);
 
     Arc::new(db.into_connection())
 }
 
 fn oauth_router(db: Arc<DbConn>) -> Router {
     Router::new()
-        .route("/authorize", get(authorize))
+        .route("/authorize", post(authorize))
         .with_state(db)
 }
 
