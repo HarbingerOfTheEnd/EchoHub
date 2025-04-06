877d04022d2b51b2e2b61583b68948f17d348b79
Sai Kumar <ksai44576@gmail.com>
2025-04-06 16:34:25 +0530

    feat: add entity module with user model and dependencies

---
+++ backend/entity/src/user.rs
@@ -0,0 +1,21 @@
+use sea_orm::prelude::*;
+
+#[derive(Clone, Debug, DeriveEntityModel, Eq, PartialEq)]
+#[sea_orm(table_name = "users")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: u64,
+    #[sea_orm(unique)]
+    pub username: String,
+    #[sea_orm(unique)]
+    pub email: String,
+    pub discriminator: u32,
+    pub password: String,
+    pub email_verified: bool,
+    pub created_at: DateTime,
+}
+
+#[derive(Clone, Debug, DeriveRelation, EnumIter)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
