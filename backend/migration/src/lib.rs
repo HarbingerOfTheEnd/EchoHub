33cd53e944ab7b7a775371493f5c00af1b548d42
Sai Kumar <ksai44576@gmail.com>
2025-04-17 11:03:21 +0530

    refactor: remove obsolete migration for user table and update migrator

--- backend/migration/src/lib.rs
+++ backend/migration/src/lib.rs
@@ -1,6 +1,5 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20250406_110838_create_table;
 mod m20250407_172424_create_table;
 
 pub struct Migrator;
@@ -8,9 +7,6 @@ pub struct Migrator;
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![
-            Box::new(m20250406_110838_create_table::Migration),
-            Box::new(m20250407_172424_create_table::Migration),
-        ]
+        vec![Box::new(m20250407_172424_create_table::Migration)]
     }
 }
