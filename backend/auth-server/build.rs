fb5a634870a788bac4f855429f800a0feda6e6dc
Sai Kumar <ksai44576@gmail.com>
2025-05-10 23:29:17 +0530

    fix: update proto path to include versioning

--- backend/auth-server/build.rs
+++ backend/auth-server/build.rs
@@ -3,7 +3,7 @@ use std::{env::var, error::Error, path::Path};
 use tonic_build::configure;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let protos = &["protos/auth.proto"];
+    let protos = &["protos/v1/auth.proto"];
     let includes = &["protos", "protos/google"];
     let out_dir = var("OUT_DIR").unwrap();
     let descriptor_path = Path::new(&out_dir).join("auth_descriptor.bin");
