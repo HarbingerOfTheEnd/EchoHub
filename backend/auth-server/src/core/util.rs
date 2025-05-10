d2384e6216549cf3e1b49ce9176d026089ad05ce
Sai Kumar <ksai44576@gmail.com>
2025-05-10 20:38:25 +0530

    feat: add token generation functions for user authentication

--- backend/auth-server/src/core/util.rs
+++ backend/auth-server/src/core/util.rs
@@ -1,12 +1,20 @@
+use core::hash;
 use std::{
     env::var,
     sync::atomic::{AtomicU64, Ordering::SeqCst},
+    thread::current,
 };
 
+use base64::{Engine, engine::general_purpose::STANDARD};
+use entity::users;
 use lazy_static::lazy_static;
+use sha2::{Digest, Sha256};
 use time::OffsetDateTime;
+use uuid::Uuid;
 
 const EPOCH: u64 = 1_735_689_600_000;
+pub const ACCESS_TOKEN_EXPIRES_IN: u64 = 60 * 60 * 24 * 30 * 2;
+pub const REFRESH_TOKEN_EXPIRES_IN: u64 = 60 * 60 * 24 * 30 * 6;
 lazy_static! {
     static ref SNOWFLAKE_BASE: u64 = {
         let worker = var("EH_WORKER_ID")
@@ -28,4 +36,31 @@ pub fn generate_snowflake(now: u64) -> String {
     let timestamp = (now - EPOCH) << 22;
     let id = *SNOWFLAKE_BASE + seq + timestamp;
     id.to_string()
+}
+
+pub fn generate_token(user: &users::Model) -> String {
+    let user_id_part = STANDARD.encode(user.id.as_bytes());
+    let unique_part = format!(
+        "{}{}",
+        current().name().unwrap_or("unnamed"),
+        OffsetDateTime::now_utc().unix_timestamp(),
+    );
+    let unique_part = STANDARD.encode(unique_part.as_bytes());
+    let random_part = Uuid::new_v4().simple().to_string();
+
+    let mut hasher = Sha256::new();
+    hasher.update(unique_part);
+    hasher.update(b".");
+    hasher.update(random_part);
+    let hash = STANDARD.encode(hasher.finalize().as_slice());
+
+    format!("{user_id_part}.{hash}")
+        .replace("+", "_")
+        .replace("/", "-")
+}
+
+pub fn generate_token_pair(user: &users::Model) -> (String, String) {
+    let access_token = generate_token(user);
+    let refresh_token = generate_token(user);
+    (access_token, refresh_token)
 }
