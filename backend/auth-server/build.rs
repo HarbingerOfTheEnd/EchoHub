c613cb87ac0146f64cf5cd8a46ac263121924e7a
Sai Kumar <ksai44576@gmail.com>
2025-05-10 23:22:28 +0530

    refactor: simplify proto paths in build.rs for clarity

--- backend/auth-server/build.rs
+++ backend/auth-server/build.rs
@@ -3,10 +3,8 @@ use std::{env::var, error::Error, path::Path};
 use tonic_build::configure;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let protos = &[Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("protos")
-        .join("auth.proto")];
-    let includes = &[Path::new(env!("CARGO_MANIFEST_DIR")).join("protos")];
+    let protos = &["protos/auth.proto"];
+    let includes = &["protos", "protos/google"];
     let out_dir = var("OUT_DIR").unwrap();
     let descriptor_path = Path::new(&out_dir).join("auth_descriptor.bin");
 
