4c5a7c9b1c8659253e53d2cd4b930cba56b5355a
Sai Kumar <ksai44576@gmail.com>
2025-05-20 11:48:02 +0530

    feat: implement gRPC client structure for authentication service

---
+++ backend/gateway-server/src/v1/auth.rs
@@ -0,0 +1,22 @@
+use std::env::var;
+
+use anyhow::{Context, Result};
+use auth_service_client::AuthServiceClient;
+use tonic::{include_proto, transport::Channel};
+
+include_proto!("v1.auth");
+
+pub(crate) struct GrpcClients {
+    pub(crate) auth_client: AuthServiceClient<Channel>,
+}
+
+impl GrpcClients {
+    pub(crate) async fn new() -> Result<Self> {
+        let auth_service_url = var("AUTH_SERVICE_URL").context("AUTH_SERVICE_URL not set")?;
+        let auth_client = AuthServiceClient::connect(auth_service_url)
+            .await
+            .context("Failed to connect to auth service")?;
+
+        Self { auth_client }
+    }
+}
