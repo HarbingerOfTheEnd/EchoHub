592a1b27ec5b207a3cddb3754e85a2ee6b923d5f
Sai Kumar <ksai44576@gmail.com>
2025-05-20 12:56:34 +0530

    feat: implement main function with GraphQL server setup and gRPC client integration

--- backend/gateway-server/src/main.rs
+++ backend/gateway-server/src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env::var, net::SocketAddr, path::Path};
+
+use anyhow::{Context, Result};
+use async_graphql::{EmptyMutation, Schema, http::GraphiQLSource};
+use async_graphql_axum::{GraphQLRequest, GraphQLResponse, GraphQLSubscription};
+use axum::{
+    Extension, Router,
+    response::{Html, IntoResponse},
+    routing::{get, get_service},
+};
+use axum_server::Server;
+use dotenvy::from_filename;
+use schema::Subscription;
+#[macro_use]
+extern crate tracing;
+use tracing_subscriber::{EnvFilter, fmt};
+use v1::auth::GrpcClient;
+
+use crate::schema::Query;
+
+mod schema;
+mod v1;
+
+type GatewaySchema = Schema<Query, EmptyMutation, Subscription>;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let env_path = Path::new(env!("CARGO_MANIFEST_DIR")).join(".env");
+    from_filename(env_path).context("Failed to load .env file")?;
+
+    fmt()
+        .with_target(true)
+        .with_level(true)
+        .with_ansi(true)
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let address = var("ADDRESS")
+        .context("ADDRESS not set")?
+        .parse::<SocketAddr>()
+        .context("Failed to parse ADDRESS")?;
+
+    let grpc_client = GrpcClient::new()
+        .await
+        .context("Failed to create gRPC client")?;
+    let schema = Schema::build(Query, EmptyMutation, Subscription)
+        .data(grpc_client)
+        .finish();
+
+    let router = Router::new()
+        .route("/graphql", get(graphql_playground).post(graphql_handler))
+        .route_service(
+            "/subscriptions",
+            get_service(GraphQLSubscription::new(schema.clone())),
+        )
+        .layer(Extension(schema));
+
+    Server::bind(address)
+        .serve(router.into_make_service())
+        .await
+        .context("Failed to start server")
+}
+
+async fn graphql_playground() -> impl IntoResponse {
+    Html(
+        GraphiQLSource::build()
+            .endpoint("/graphql")
+            .subscription_endpoint("/subscriptions")
+            .finish(),
+    )
+}
+
+async fn graphql_handler(
+    Extension(schema): Extension<GatewaySchema>,
+    request: GraphQLRequest,
+) -> GraphQLResponse {
+    schema.execute(request.into_inner()).await.into()
 }
