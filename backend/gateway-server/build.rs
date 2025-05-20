f9ef2bdb5886fa081bf9eeb1cfa72cc7c25b309d
Sai Kumar <ksai44576@gmail.com>
2025-05-20 19:09:28 +0530

    fix: remove unused file descriptor set path in build configuration

--- backend/gateway-server/build.rs
+++ backend/gateway-server/build.rs
@@ -1,17 +1,14 @@
-use std::{env::var, error::Error, path::Path};
+use std::error::Error;
 
 use tonic_build::configure;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let protos = &["../auth-server/protos/v1/auth.proto"];
     let includes = &["../auth-server/protos", "../auth-server/protos/google"];
-    let out_dir = var("OUT_DIR").unwrap();
-    let descriptor_path = Path::new(&out_dir).join("gateway_descriptor.bin");
 
     configure()
         .build_client(true)
         .build_server(false)
-        .file_descriptor_set_path(descriptor_path)
         .compile_protos(protos, includes)?;
 
     Ok(())
