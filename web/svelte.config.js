83b36d29a298a63c5a3a21a0f0df0afadd80933c
Sai Kumar <ksai44576@gmail.com>
2025-06-03 14:29:45 +0530

    chore: remove unused import of sveltePreprocess from svelte.config.js

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { sveltePreprocess } from 'svelte-preprocess';
 
 const config = {
     preprocess: [vitePreprocess()],
